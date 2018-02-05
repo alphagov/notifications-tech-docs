ceffaf5d71caee28aa742773e6936ac513d4953d
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2018-02-05 13:55:59 +0000

    Add page for each client
    These pages all use the same template with the relevant client doc
    rendered.

--- config.rb
+++ config.rb
@@ -1,3 +1,5 @@
+require 'app/client_docs'
+require 'app/external_doc'
 require 'lib/unique_identifier_generator'
 require 'lib/unique_identifier_extension'
 require 'lib/tech_docs_html_renderer'
@@ -25,9 +27,11 @@ page '/*.txt', layout: false
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
+ClientDocs.pages.each do |page|
+  proxy "/#{page.client}.html", "client_template.html", locals: {
+    repo: page.repository
+  }, ignore: true
+end
 
 # General configuration
 
