8bc6d8b59bd815c107f89059cdaf0df67e4bfbf6
Tom Byers <tombaromba@gmail.com>
2019-12-06 16:03:03 +0000

    Change to trial new heading structure
    Note: only works for Node client

--- app/external_doc.rb
+++ app/external_doc.rb
@@ -2,7 +2,7 @@ require "faraday"
 
 class ExternalDoc
   def self.fetch(repository:)
-    url = "https://raw.githubusercontent.com/#{repository}/master/DOCUMENTATION.md"
+    url = "https://raw.githubusercontent.com/#{repository}/fix-heading-structure/DOCUMENTATION.md"
 
     response = Faraday.get(url)
 
